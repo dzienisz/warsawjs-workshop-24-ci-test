72d61b80e137811d1b32d669abe1460f60c7233b
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2018-09-16 15:05:41 +0200

    Add cache

--- service/ProductsService.js
+++ service/ProductsService.js
@@ -1,14 +1,30 @@
-import fetch from 'node-fetch'
-
 class ProductsService {
+  constructor(params) {
+    this.products = "http://localhost:3000/products"
+    this._cacheInterval = 5 * 30 * 6000
+    this._cacheValidityDate
+  }
 
-    products = 'http://localhost:3000/products';
-
-    async fetchList() {
-        return await fetch(this.products)
-            .then(body => body.json());
+  async fetchList() {
+    if (Date.now() < this._cacheValidityDate) {
+        console.log('chached data')
+        return await this._products
     }
+    
+    return await this._fetcher(this.products).then(body => {
+        console.log('new data')
+        this._products = body.json()
+        this._cacheValidityDate = Date.now() + this._cacheInterval
+        return this._products
+    });
+  }
 
+  setFetcher = fetch => {
+    this._fetcher = fetch
+    return this;
+  };
 }
 
-export default new ProductsService();
+const service = new ProductsService()
+
+export default fetch => service.setFetcher(fetch)
--- test.js
+++ test.js
@@ -1,11 +1,18 @@
-import ProductsService from './service/ProductsService'
+import { default as ProductsService } from './service/ProductsService'
+
+const service = ProductsService(require('node-fetch'))
 
 test('is exist', () => {
-    expect(typeof ProductsService).toBe('object');
+    expect(typeof service).toBe('object')
 });
 
 test('fetching', async () => {
-    const products = await ProductsService.fetchList();
-    expect(Array.isArray(products)).toBeTruthy();
-    expect(products.length).toBeGreaterThan(0);
+    const products = await service.fetchList()
+    expect(Array.isArray(products)).toBeTruthy()
+    expect(products.length).toBeGreaterThan(0)
+});
+
+test('fetching cached data', async () => {
+    const products = await service.fetchList()
+    expect(products.length).toBeGreaterThan(0)
 });
